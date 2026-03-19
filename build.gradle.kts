c2f85495a9c60910fcc867ef4b7a3386d86ad56b
r0man31 <leshchuh31@gmail.com>
2026-03-19 16:53:33 +0200

    Fix JitPack root publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,19 +17,41 @@ plugins {
     id("com.android.library") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
+    `maven-publish`
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
 
-// JitPack expects a root-level publishToMavenLocal task.
-tasks.register("publishToMavenLocal") {
-    dependsOn(":ethora-component:publishReleasePublicationToMavenLocal")
-}
-
 allprojects {
     group = (findProperty("group") as String?) ?: "com.github.dappros"
     version = (findProperty("version") as String?) ?: "local-SNAPSHOT"
     layout.buildDirectory.set(file("/tmp/android_build/${rootProject.name}/${project.name}"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("root") {
+            val resolvedGroupId = project.group.toString()
+            val resolvedVersion = project.version.toString()
+            val moduleGroupId = "$resolvedGroupId.ethora-sdk-android"
+
+            groupId = resolvedGroupId
+            artifactId = "ethora-sdk-android"
+            version = resolvedVersion
+            pom.packaging = "pom"
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                val dependencyNode = dependenciesNode.appendNode("dependency")
+                dependencyNode.appendNode("groupId", moduleGroupId)
+                dependencyNode.appendNode("artifactId", "ethora-component")
+                dependencyNode.appendNode("version", resolvedVersion)
+            }
+        }
+    }
+}
+
+tasks.named("publishToMavenLocal") {
+    dependsOn(":ethora-component:publishReleasePublicationToMavenLocal")
 }
